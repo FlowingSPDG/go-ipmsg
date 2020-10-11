662d6093c30a6fde5b98a83bf3fa7ceeafe547c1
Shugo Kawamura <kawamura@giken.tv>
2020-10-12 02:51:03 +0900

    Fix print

--- example/main.go
+++ example/main.go
@@ -24,17 +24,18 @@ func main() {
 	input := make(chan string)
 	next := make(chan string)
 	quit := make(chan struct{})
-	recv := make(chan string)
+	recv := make(chan goipmsg.ClientData)
 	// main loop or receive event from channel
 	go func() {
 		for {
 			var str string
+			var cd goipmsg.ClientData
 			select {
 			case str = <-input:
 				SwitchInput(ipmsg, str, quit)
 				next <- ""
-			case str = <-recv:
-				pp.Println("recv=", str)
+			case cd = <-recv:
+				pp.Println("recv=", cd)
 			}
 		}
 	}()
@@ -58,7 +59,7 @@ func main() {
 				panic(err)
 			}
 			//pp.Println("recv=", cd.String())
-			recv <- cd.Option
+			recv <- *cd
 		}
 	}()
 
